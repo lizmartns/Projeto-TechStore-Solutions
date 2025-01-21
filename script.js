f8136b681d5c5f9d8e4ec15d16dd28daaabaf509
Liz Martins <martins.flordeliz@gmail.com>
2025-01-21 10:14:07 -0300

    update

---
+++ script.js
@@ -0,0 +1,18 @@
+class Usuario{
+    nome;
+    email;
+    senha;
+    constructor(nome, email, senha){
+        this.nome = nome
+        this.email = email
+        this.senha = senha 
+    }
+}
+var listaUsuarios = [
+    new Usuario('João', 'joao@gmail.com', '123456'),
+    new Usuario('Maria', 'maria@gmail.com', '098765'),
+    new Usuario('Pedro', 'pedro@gmail.com', '153289');
+]
+ 
+
+console.log(joao)
